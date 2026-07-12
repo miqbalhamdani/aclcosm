27f456320d9aea0a8187b52a222791467d90b167
Iqbal Hamdani <miqbalh94@gmail.com>
2026-07-12 14:21:28 +0700

    feat: remove npm install and build to ci/cd and add tracking by microsoft clarity

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -52,6 +52,16 @@
     <link rel="stylesheet" type="text/css" href="{{asset('vue/css/app.css')}}">
     <!-- END: Laravel Vue-->
 
+    {{-- Microsoft Clarity --}}
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xl5gs45tba");
+    </script>
+    {{-- Microsoft Clarity --}}
+
     @stack('css')
   </head>
   <body>
